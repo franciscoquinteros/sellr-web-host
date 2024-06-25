feb42a5eb9ded8fd6c7ec5de6cb8e51a8656e9d8
franciscoquinteros <franciscoquinterosok@gmail.com>
2024-06-25 12:18:21 -0300

    Add mpp

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -336,14 +336,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "d2a6204e7a2a6c02be9cd3d9a37add45
 "env": "6e130a56cd49742b84c25bec5abed0c2",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "17ad9aedce73cb12c979a20ae17be1ef",
+"flutter_bootstrap.js": "fe6811a96dcfffd3fc9a30e14fe9af2b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1daf77984fd9ce00c468294c30a261ed",
-"/": "1daf77984fd9ce00c468294c30a261ed",
-"main.dart.js": "ee5e79762a442bb8fd1631b5d5dedf4d",
+"index.html": "4e4437b124d8f74d350894752fd69137",
+"/": "4e4437b124d8f74d350894752fd69137",
+"main.dart.js": "7de93a13afd999bc5eb518c68bed5178",
 "manifest.json": "a029b0cfed3487c27fbe5db38e5533ce",
 "version.json": "679d9d140db6f376c76814417c3cf59f"};
 // The application shell files that are downloaded before a service worker can
